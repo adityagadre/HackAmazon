ddd98b0372aa5656519257d288b49b09e0c508de
tbq <truongbaquan@gmail.com>
2014-06-27 21:33:21 -0700

    After tuning parameter for Gaussian

--- src/hack.py
+++ src/hack.py
@@ -6,14 +6,15 @@ Created on Jun 27, 2014
 
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
+#from matplotlib import pyplot as plt
 
 debug = False
 
 def preprocess(img):
-    kernel = np.ones((5,5),np.float32)/25
-    filtered = cv2.filter2D(img,-1,kernel)
-    #filtered = cv2.GaussianBlur(img,(5,5),0)
+    sz = 9
+    kernel = np.ones((sz,sz),np.float32)/(sz*sz)
+    #filtered = cv2.filter2D(img,-1,kernel)
+    filtered = cv2.GaussianBlur(img,(sz,sz),0)
     
     descriptorExtractor = cv2.SIFT()
     
@@ -28,20 +29,20 @@ def hack(imgFile1, imgFile2):
     kp1, desc1, filtered1, imgProcessed1 = preprocess(img1)
     kp2, desc2, filtered2, imgProcessed2 = preprocess(img2)
     
-    if debug:
-        plt.subplot(231),plt.imshow(img1),plt.title('Original')
-        plt.xticks([]), plt.yticks([])
-        plt.subplot(232),plt.imshow(filtered1),plt.title('Filtered')
-        plt.xticks([]), plt.yticks([])
-        plt.subplot(233),plt.imshow(imgProcessed1),plt.title('Keypoints')
-        plt.xticks([]), plt.yticks([])
-        plt.subplot(234),plt.imshow(img2),plt.title('Original')
-        plt.xticks([]), plt.yticks([])
-        plt.subplot(235),plt.imshow(filtered2),plt.title('Filtered')
-        plt.xticks([]), plt.yticks([])
-        plt.subplot(236),plt.imshow(imgProcessed2),plt.title('Keypoints')
-        plt.xticks([]), plt.yticks([])
-        plt.show()
+#     if debug:
+#         plt.subplot(231),plt.imshow(img1),plt.title('Original')
+#         plt.xticks([]), plt.yticks([])
+#         plt.subplot(232),plt.imshow(filtered1),plt.title('Filtered')
+#         plt.xticks([]), plt.yticks([])
+#         plt.subplot(233),plt.imshow(imgProcessed1),plt.title('Keypoints')
+#         plt.xticks([]), plt.yticks([])
+#         plt.subplot(234),plt.imshow(img2),plt.title('Original')
+#         plt.xticks([]), plt.yticks([])
+#         plt.subplot(235),plt.imshow(filtered2),plt.title('Filtered')
+#         plt.xticks([]), plt.yticks([])
+#         plt.subplot(236),plt.imshow(imgProcessed2),plt.title('Keypoints')
+#         plt.xticks([]), plt.yticks([])
+#         plt.show()
     
     # create BFMatcher object
     bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck = True)
@@ -50,4 +51,4 @@ def hack(imgFile1, imgFile2):
     return 100 * (1 - float(len(matches)) / max(len(desc1), len(desc2)))
 
 #print hack('../distrib/set1/unchanged/pair_0000_inbound.jpg', '../distrib/set1/unchanged/pair_0000_outbound.jpg')
-print hack('../distrib/set2/unchanged/pair_0000_before.jpg', '../distrib/set2/unchanged/pair_0000_later.jpg')
+#print hack('../distrib/set2/unchanged/pair_0000_before.jpg', '../distrib/set2/unchanged/pair_0000_later.jpg')
